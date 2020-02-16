31d260c6fbda958ee62fa968ce5ec656f5c3b044
Paulo Lopes <paulopes00@hotmail.com>
2020-02-16 12:53:16 -0500

    Remove top level imports

--- panel_components/__init__.py
+++ panel_components/__init__.py
@@ -1,6 +1,1 @@
 # -*- coding: utf-8 -*-
-
-from .component import Component
-from .file import file
-from .plt_snapshot import plt_snapshot
-from .utils import IS_A_JUPYTER_NOTEBOOK, is_a_number, template_escape
