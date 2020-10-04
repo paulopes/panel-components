55cf1c9a2f062e4f08a1945b9fa34ba3765f1d32
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2020-10-04 10:56:40 +0200

    blackified

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,58 +1,84 @@
-from panel_components import file
-import pathlib
-import pytest
-from panel_components.utils import IS_A_JUPYTER_NOTEBOOK, is_a_number, template_escape, get_dir_name, _read_file, find_src_file
 import os
+import pathlib
+
+import pytest
+from panel_components import file
+from panel_components.utils import (
+    IS_A_JUPYTER_NOTEBOOK,
+    _read_file,
+    find_src_file,
+    get_dir_name,
+    is_a_number,
+    template_escape,
+)
+
 
 def test_is_a_jupyter_notebook():
     assert not IS_A_JUPYTER_NOTEBOOK
 
-@pytest.mark.parametrize(["value", "expected"], [
-    (0, True),
-    (1, True),
-    (1.1, True),
-    ("test", False),
-    (is_a_number, False),
-    (pytest, False),
-])
+
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        (0, True),
+        (1, True),
+        (1.1, True),
+        ("test", False),
+        (is_a_number, False),
+        (pytest, False),
+    ],
+)
 def test_is_a_number(value, expected):
     assert is_a_number(value) is expected
 
-@pytest.mark.parametrize(["value", "expected"], [
-    ("{{ value }}", "{{'{{'}} value }}"),
-    ("{% value }", "{{'{%'}} value }"),
-    ("{# value }", "{{'{#'}} value }"),
-])
-def test_template_escape(value, expected):
-    assert template_escape(value)==expected
 
-@pytest.mark.parametrize(["folder", "expected"], [
-    (None, os.getcwd()),
-    ("/c/repos/panel-components", "panel-components"),
-    ("/c/repos/panel-components/", "panel-components"),
-])
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        ("{{ value }}", "{{'{{'}} value }}"),
+        ("{% value }", "{{'{%'}} value }"),
+        ("{# value }", "{{'{#'}} value }"),
+    ],
+)
+def test_template_escape(value, expected):
+    assert template_escape(value) == expected
+
+
+@pytest.mark.parametrize(
+    ["folder", "expected"],
+    [
+        (None, os.getcwd()),
+        ("/c/repos/panel-components", "panel-components"),
+        ("/c/repos/panel-components/", "panel-components"),
+    ],
+)
 def test_get_dir_name(folder, expected):
-    assert get_dir_name(folder)==expected
+    assert get_dir_name(folder) == expected
+
 
 def test_read_file_none():
-    assert _read_file(None)==""
+    assert _read_file(None) == ""
+
 
 def test_read_file_not_a_file():
-    assert _read_file("this.file.does.not.exist")==""
+    assert _read_file("this.file.does.not.exist") == ""
+
 
 def test_read_file_when_a_directory():
-    assert _read_file(os.getcwd())==""
+    assert _read_file(os.getcwd()) == ""
+
 
 def test_read_file(tmp_path: pathlib.Path):
     # When
     file = tmp_path / "tmp.txt"
     file.write_text("hello world")
     # Then
-    assert _read_file(str(file))=="hello world"
+    assert _read_file(str(file)) == "hello world"
+
 
 def test_find_src_file(tmp_path: pathlib.Path):
     filename = "image.png"
-    src_folder_path = tmp_path /"src"
+    src_folder_path = tmp_path / "src"
     src_folder_path.mkdir(parents=True, exist_ok=True)
     src_folder = str(src_folder_path)
 
